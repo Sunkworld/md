d610ecc2bfd7b4456850dc92c9546f5bb5c31a01
sunkworld <sunkworld@yandex.com>
2016-09-15 15:42:21 +0800

    updated

--- plot.sh
+++ plot.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-f="q_mp.txt"
-v="H-H"
-nr=`cat $f | wc -l`
+f="baoab.txt"
+g="mp.txt"
 # write the file for gnuplot
 echo "set term png enhanced
 
 set output '${f}.png'
-set xlabel 'x'
-set ylabel 'p(x)'
-set xrange[-3:3]
-set yrange[0:0.5]
-gauss(x)=1/sqrt(2*pi)*exp(-x*x/2)
-bin_width=0.1 # set bin width    
-bin_num(x)=floor(x/bin_width)  
-rounded(x)=bin_width*(bin_num(x) + 0.5)
-plot '${f}' u (rounded(\$1)):(1/bin_width/$nr) t 'mp(gamma=1)' smooth frequency with lines#, gauss(x) t 'Exact'
-" |  gnuplot
+set xlabel 'dt'
+set ylabel 'E_p'
+
+f(x)=0.404715
+plot '${f}' u 1:3:4 t 'baoab' with yerrorlines, '${g}' u 1:3:4 t 'mp' with yerrorlines, f(x)
+ " |  gnuplot
