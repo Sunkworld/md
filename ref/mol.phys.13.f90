08408c1ad4b759a4e845b057cef2c7a62e054293
Sunkworld <sunkworld@yandex.com>
2016-10-07 08:14:32 +0000

    ...

--- ref/mol.phys.13.f90
+++ ref/mol.phys.13.f90
@@ -200,7 +200,7 @@ subroutine molphys
        ek(j) = ek(j)/tsstep
        ep2(j) = ep2(j)/tsstep
        ek2(j) = ek2(j)/tsstep
-       write(100,'(I8,F16.8,F16.8,F16.8)') j, ep(j), ek(j), ep2(j), ek2(j)
+       write(100,'(I8,F16.8,F16.8,F16.8,F16.8)') j, ep(j), ek(j), ep2(j), ek2(j)
     end do
     ep_ave = sum(ep)/sample
     ep_std = sqrt(sum((ep-ep_ave)**2)/(sample-1)/sample)
