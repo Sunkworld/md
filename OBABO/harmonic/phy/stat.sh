cd2e607719d9e3972bbf89342b99bfcdb47c53e7
lab <1300011708@pku.edu.cn>
2017-03-21 14:15:26 +0800

    minor modification

--- OBABO/harmonic/phy/stat.sh
+++ OBABO/harmonic/phy/stat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 gammalist='0.01 0.02 0.05 0.1 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 4.0 10 20 40 50 100 200 300 1000 10000'
-gammalist='0.01'
 for gamma in  $gammalist
 do
   for t in 0.02 0.05 0.1
